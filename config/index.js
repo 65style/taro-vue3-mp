28aabbe8814966eeeb4b546a9a093b892da5c9d3
liyangming <lym210110@gmail.com>
2022-09-25 22:17:44 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 const config = {
   projectName: 'taro-vue3-mp',
   date: '2022-9-25',
@@ -12,23 +12,18 @@ const config = {
   sourceRoot: 'src',
   outputRoot: 'dist',
   plugins: ['@tarojs/plugin-html'],
-  defineConstants: {
-  },
+  defineConstants: {},
   copy: {
-    patterns: [
-    ],
-    options: {
-    }
+    patterns: [],
+    options: {}
   },
   framework: 'vue3',
   compiler: 'webpack5',
   cache: {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
-  sass:{
-    resource: [
-			path.resolve(__dirname, '..', 'src/assets/styles/custom_theme.scss')
-		],
+  sass: {
+    resource: [path.resolve(__dirname, '..', 'src/assets/styles/custom_theme.scss')],
     data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
   },
   mini: {
@@ -61,8 +56,7 @@ const config = {
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {
-        }
+        config: {}
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
@@ -75,7 +69,7 @@ const config = {
   }
 }
 
-module.exports = function (merge) {
+module.exports = function(merge) {
   if (process.env.NODE_ENV === 'development') {
     return merge({}, config, require('./dev'))
   }
